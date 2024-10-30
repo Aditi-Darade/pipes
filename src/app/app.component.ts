a4bb0f53cedd66eff570935c8a3a9d99e0b804f5
Aditi-Darade <aditidarade1@gmail.com>
2024-10-30 15:21:10 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipes';
+
+  name = "";
+  date = "";
+  amount = 0; //Don't know how else to declare the variable amount of type integer
+  onNameChange(value: string)
+  {
+    this.name = value;
+  }
+
+  onDateChange(value: string)
+  {
+    this.date = value;
+  }
+
+  onAmountChange(value: string)
+  {
+    this.amount = parseFloat(value);
+  }
 }
